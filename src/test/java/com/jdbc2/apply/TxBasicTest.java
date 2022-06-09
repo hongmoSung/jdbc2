6e9eba73c611d77faad62a7f1711e29b510bb3d1
hongmoSung <tesung30@naver.com>
2022-06-09 21:33:03 +0900

    트랜잭션 적용 확인

---
+++ src/test/java/com/jdbc2/apply/TxBasicTest.java
@@ -0,0 +1,59 @@
+package com.jdbc2.apply;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Slf4j
+@SpringBootTest
+public class TxBasicTest {
+
+    @Autowired
+    BasicService basicService;
+
+    @Test
+    void proxyCheck() {
+        log.info("aop class={}", basicService.getClass());
+        assertThat(AopUtils.isAopProxy(basicService)).isTrue();
+    }
+
+    @Test
+    void txTest() {
+        basicService.tx();
+        basicService.nonTx();
+    }
+
+    @TestConfiguration
+    static class TxApplyBasicConfig {
+        @Bean
+        public BasicService basicService() {
+            return new BasicService();
+        }
+    }
+
+    @Slf4j
+    static class BasicService {
+
+        @Transactional
+        public void tx() {
+            log.info("tx");
+            boolean transactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active {}", transactionActive);
+        }
+
+        public void nonTx() {
+            log.info("none tx");
+            boolean transactionActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("tx active {}", transactionActive);
+        }
+
+    }
+}
