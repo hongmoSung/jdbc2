9fc3a034b1c3f25303dbdec26bc009fb28dc9c56
hongmoSung <tesung30@naver.com>
2022-06-09 20:27:34 +0900

    initial commit

---
+++ src/main/java/com/jdbc2/Jdbc2Application.java
@@ -0,0 +1,13 @@
+package com.jdbc2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdbc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jdbc2Application.class, args);
+	}
+
+}
