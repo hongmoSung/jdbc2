8272b25539f8f4184ec447a2b5c105aab606b034
hongmoSung <tesung30@naver.com>
2022-06-11 12:48:51 +0900

    스프링 트랜잭션 전파4- 전파 예제

--- src/test/java/com/jdbc2/propagation/BasicTxTest.java
+++ src/test/java/com/jdbc2/propagation/BasicTxTest.java
@@ -75,4 +75,20 @@ public class BasicTxTest {
         txManager.rollback(status2);
         log.info("트랜잭션 롤백 완료 ");
     }
+
+    @Test
+    void inner_commit() {
+        log.info("외부 트랜잭션 시작");
+        TransactionStatus outer = txManager.getTransaction(new
+                DefaultTransactionAttribute());
+        log.info("outer.isNewTransaction()={}", outer.isNewTransaction());
+        log.info("내부 트랜잭션 시작");
+        TransactionStatus inner = txManager.getTransaction(new
+                DefaultTransactionAttribute());
+        log.info("inner.isNewTransaction()={}", inner.isNewTransaction());
+        log.info("내부 트랜잭션 커밋");
+        txManager.commit(inner);
+        log.info("외부 트랜잭션 커밋");
+        txManager.commit(outer);
+    }
 }
